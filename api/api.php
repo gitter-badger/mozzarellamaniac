85c027daf7a60586b447a208a9d327f0a435f563
valenb <valen.bozoki@gmail.com>
2015-10-18 17:56:20 +0200

    api.php
    declaration of function GetAllPizza(), and the beginning of GetPizzaById

---
+++ api/api.php
@@ -0,0 +1,45 @@
+<?php
+	function GetAllPizza(){
+		//Establishing Mysql connection
+		$conn = new PDO("mysql:host=localhost;dbname=pizzeria", 'root', 'godfather');
+		
+		$sql = "SELECT pizza_id, name, price FROM pizza";
+				
+		$query = $conn->query($sql);
+		
+		while($res = $query->fetch(PDO::FETCH_ASSOC)) {
+			$rows[] = $res;
+		}
+		
+		echo json_encode($rows) . "<br /><br />";
+	}
+	
+	function GetPizzaById($id=null){
+		$id = $_GET['id'];
+		if(!$id){
+			echo "<script type='text/javascript'>alert('Nincs megadva pizza azonosítószám!');</script>";
+		}else{
+			//Establishing Mysql connection
+			$conn = new PDO("mysql:host=localhost;dbname=pizzeria", 'root', 'godfather');
+			
+			$sql = "SELECT pizza_id, name, price FROM pizza WHERE pizza_id = $id";
+			
+			$sql2 = "SELECT name topping FROM topping";
+			
+			$query = $conn->query($sql);
+			$query2 = $conn->query($sql2);
+			while($res = $query2->fetch(PDO::FETCH_ASSOC)) {
+					$row[] = $res;
+			}
+			print_r($row);
+			while($res = $query->fetch(PDO::FETCH_ASSOC)) {
+				$rows[] = $res;
+			}
+			$rows[] = $row;
+			
+			echo json_encode($rows);
+		}
+	}
+	GetAllPizza();
+	GetPizzaById();
+?>
