0eea2f275a761b0d9ea55d09292a3489b6476951
valenb <valen.bozoki@gmail.com>
2015-10-18 21:45:54 +0200

    Finished GetPizzaById function + code cleanup

--- api/api.php
+++ api/api.php
@@ -1,43 +1,49 @@
 <?php
 	function GetAllPizza(){
 		//Establishing Mysql connection
-
-		header("Content-type: application/json; charset=utf-8");
-		$conn = new PDO("mysql:host=localhost;dbname=pizzeria", 'root', 'root');
-
+		$conn = new PDO("mysql:host=localhost;dbname=pizzeria", 'root', 'godfather');
 		
 		$sql = "SELECT pizza_id, name, price, image FROM pizza";
 				
 		$query = $conn->query($sql);
+		
 		while($res = $query->fetch(PDO::FETCH_ASSOC)) {
 			$rows[] = $res;
 		}
-		echo json_encode($rows) . "";
+		
+		echo json_encode($rows);
 	}
 	
 	function GetPizzaById($id=null){
 		$id = $_GET['id'];
 		if(!$id){
-			//echo "<script type='text/javascript'>alert('Nincs megadva pizza azonosítószám!');</script>";
+			echo "<script type='text/javascript'>alert('Nincs megadva pizza azonosítószám!');</script>";
 		}else{
 			//Establishing Mysql connection
-			$conn = new PDO("mysql:host=localhost;dbname=pizzeria", 'root', 'root');
+			$conn = new PDO("mysql:host=localhost;dbname=pizzeria", 'root', 'godfather');
 			
-			$sql = "SELECT pizza_id, name, price FROM pizza WHERE pizza_id = $id";
+			$sql = "SELECT pizza_id, name, price, image FROM pizza WHERE pizza_id = $id";
 			
 			$sql2 = "SELECT name topping FROM topping";
 			
+			$row = "";
+			
 			$query = $conn->query($sql);
+			
 			$query2 = $conn->query($sql2);
-			while($res = $query2->fetch(PDO::FETCH_ASSOC)) {
-					$row[] = $res;
+			
+			while($res = $query2->fetch()) {
+					$row = $row . implode(" ",$res);
 			}
-			print_r($row);
+
 			while($res = $query->fetch(PDO::FETCH_ASSOC)) {
 				$rows[] = $res;
 			}
+			
 			$rows[] = $row;
+			
 			echo json_encode($rows);
 		}
 	}
+
 ?>
